71ab347e62c814923a45103fadb591f2fa786014
pndemo <ndemopaul1@gmail.com>
2017-12-13 09:24:07 +0300

    [Fix] Add versioning to URLs

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,6 @@ from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
 from config import app_config
 
-# pylint: disable=C0103
-
 db = SQLAlchemy()
 
 def create_app(config_name):
@@ -14,17 +12,19 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     db.init_app(app)
 
-    from app.auth import auth_blueprint
+    from app.v1.auth import auth_blueprint
     app.register_blueprint(auth_blueprint)
 
-    from app.category.views import category_view, category_specific_view, category_search_view
-    app.add_url_rule('/category/', view_func=category_view)
-    app.add_url_rule('/category/<int:category_id>', view_func=category_specific_view)
-    app.add_url_rule('/category/search', view_func=category_search_view)
+    from app.v1.category.category_views import category_view, category_specific_view, \
+            category_search_view
+    app.add_url_rule('/api/v1/category/', view_func=category_view)
+    app.add_url_rule('/api/v1/category/<int:category_id>', view_func=category_specific_view)
+    app.add_url_rule('/api/v1/category/search', view_func=category_search_view)
 
-    from app.recipe.views import recipe_view, recipe_specific_view, recipe_search_view
-    app.add_url_rule('/recipe/', view_func=recipe_view)
-    app.add_url_rule('/recipe/<int:recipe_id>', view_func=recipe_specific_view)
-    app.add_url_rule('/recipe/search', view_func=recipe_search_view)
+    from app.v1.recipe.recipe_views import recipe_view, recipe_specific_view, recipe_search_view
+    app.add_url_rule('/api/v1/recipe/<int:category_id>/', view_func=recipe_view)
+    app.add_url_rule('/api/v1/recipe/<int:category_id>/<int:recipe_id>', view_func= \
+            recipe_specific_view)
+    app.add_url_rule('/api/v1/recipe/<int:category_id>/search', view_func=recipe_search_view)
 
     return app
