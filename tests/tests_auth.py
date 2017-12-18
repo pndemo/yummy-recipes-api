b00e72f1f066a9cc38373d51331e6bf28a6111ca
pndemo <ndemopaul1@gmail.com>
2017-12-18 14:46:35 +0300

    [Update] Add more testcases

--- tests/tests_auth.py
+++ tests/tests_auth.py
@@ -11,6 +11,8 @@ class AuthTests(unittest.TestCase):
         """Define test variables and initialize app"""
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client
+        self.app_invalid = create_app(config_name="testing_invalid")
+        self.client_invalid = self.app_invalid.test_client
         self.user_data = {'email': 'example@domain.com', 'password': 'Bootcamp17'}
         with self.app.app_context():
             db.create_all()
@@ -31,6 +33,13 @@ class AuthTests(unittest.TestCase):
         result = json.loads(second_res.data.decode())
         self.assertEqual(result['message'], "Sorry, this user is already registered.")
 
+    def test_user_registration_invalid(self):
+        """Test API for invalid user registration (POST request)"""
+        res = self.client().post('/api/v1/auth/register', data=self.user_data)
+        self.assertEqual(res.status_code, 201)
+        second_res = self.client_invalid().post('/api/v1/auth/register', data=self.user_data)
+        self.assertEqual(second_res.status_code, 401)
+
     def test_user_login_registered(self):
         """Test API for user login (POST request)"""
         res = self.client().post('/api/v1/auth/register', data=self.user_data)
@@ -42,9 +51,9 @@ class AuthTests(unittest.TestCase):
         self.assertTrue(result['access_token'])
 
     def test_user_login_non_registered(self):
-        """Test API for user login (POST request)"""
-        user = {'email': 'anony@domain.com', 'password': 'bootcamp'}
-        res = self.client().post('/api/v1/auth/login', data=user)
+        """Test API for unregistered user login (POST request)"""
+        self.user_data['email'] = 'anony@domain.com'
+        res = self.client().post('/api/v1/auth/login', data=self.user_data)
         self.assertEqual(res.status_code, 401)
         result = json.loads(res.data.decode())
         self.assertEqual(result['message'], "Sorry, your email/password is invalid.")
@@ -64,6 +73,22 @@ class AuthTests(unittest.TestCase):
         result = json.loads(res.data.decode())
         self.assertEqual(result['message'], "Your password has been reset.")
 
+    def test_password_reset_invalid(self):
+        """Test API for password reset with invalid token/database credentials (POST request)"""
+        res = self.client().post('/api/v1/auth/register', data=self.user_data)
+        self.assertEqual(res.status_code, 201)
+        login_res = self.client().post('/api/v1/auth/login', data=self.user_data)
+        self.assertEqual(login_res.status_code, 200)
+        result = json.loads(login_res.data.decode())
+        access_token = result['access_token']
+        password = {'password': 'Bootcamp172'}
+        res = self.client_invalid().post('/api/v1/auth/reset_password', headers=dict(Authorization= \
+                "Bearer " + access_token), data=password)
+        self.assertEqual(res.status_code, 401)
+        res = self.client().post('/api/v1/auth/reset_password', headers=dict(Authorization= \
+                "Bearer " + access_token + 'GyHhdE'), data=password)
+        self.assertEqual(res.status_code, 401)
+
     def test_user_logout(self):
         """Test API for user logout (POST request)"""
         res = self.client().post('/api/v1/auth/register', data=self.user_data)
@@ -77,6 +102,21 @@ class AuthTests(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         result = json.loads(res.data.decode())
         self.assertEqual(result['message'], "Your have been logged out.")
+
+    def test_user_logout_invalid(self):
+        """Test API for user logout with invalid token/database credentials (POST request)"""
+        res = self.client().post('/api/v1/auth/register', data=self.user_data)
+        self.assertEqual(res.status_code, 201)
+        login_res = self.client().post('/api/v1/auth/login', data=self.user_data)
+        self.assertEqual(login_res.status_code, 200)
+        result = json.loads(login_res.data.decode())
+        access_token = result['access_token']
+        res = self.client_invalid().post('/api/v1/auth/logout', headers=dict(Authorization="Bearer " + \
+                access_token))
+        self.assertEqual(res.status_code, 401)
+        res = self.client().post('/api/v1/auth/logout', headers=dict(Authorization="Bearer " + \
+                access_token + 'GyHhdE'))
+        self.assertEqual(res.status_code, 401)
 
     def tearDown(self):
         """Teardown initialized variables"""
