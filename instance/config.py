75cdb1147060331936009da06774c582a8050f01
pndemo <ndemopaul1@gmail.com>
2018-01-13 22:24:47 +0300

    [Chore] Change application structure

---
+++ instance/config.py
@@ -0,0 +1,39 @@
+""" Configuration specifications for testing, development staging and production environments """
+
+import os
+
+class Config(object):
+    """ Parent configurations. """
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = os.getenv('SECRET')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class TestingConfig(Config):
+    """ Testing configurations. """
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/yummydb_test'
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    """ Development configurations. """
+    DEBUG = True
+
+class StagingConfig(Config):
+    """ Staging configurations. """
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """ Production configurations. """
+    DEBUG = False
+    TESTING = False
+
+# pylint: disable=C0103
+
+app_config = {
+    'testing': TestingConfig,
+    'development': DevelopmentConfig,
+    'staging': StagingConfig,
+    'production': ProductionConfig,
+}
