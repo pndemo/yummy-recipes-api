16477fa527c260e94ed10daddc335dda8c97fdbe
pndemo <ndemopaul1@gmail.com>
2018-01-13 22:22:24 +0300

    [Chore] Change application structure

--- manage.py
+++ manage.py
@@ -3,18 +3,32 @@
 # pylint: disable=W0611
 # pylint: disable=C0103
 
+import os
+
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
-from app.v1.auth import auth_models
-from app.v1.category import category_models
-from app.v1.recipe import recipe_models
+from app.v1 import models
 
 app = create_app(config_name='development')
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def create():
+    """ Command for creating main and testing databases. """
+    os.system('createdb yummydb')
+    os.system('createdb yummydb_test')
+    print('Main and testing databases created')
+
+@manager.command
+def drop():
+    """ Command for dropping main and testing databases. """
+    os.system('psql -c "DROP DATABASE IF EXISTS yummydb"')
+    os.system('psql -c "DROP DATABASE IF EXISTS yummydb_test"')
+    print('Main and testing databases dropped')
 
 if __name__ == '__main__':
     manager.run()
