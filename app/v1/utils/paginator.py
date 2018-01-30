dd90c59b3538fbde26d57877bf53c5f1ff56dff3
pndemo <ndemopaul1@gmail.com>
2018-01-30 12:52:52 +0300

    [Fix] Change pagination structure

--- app/v1/utils/paginator.py
+++ app/v1/utils/paginator.py
@@ -2,31 +2,36 @@
 
 def get_paginated_results(request, results, url):
     """ Returns previous and next pagination links. """
-    if request.values.get('start'):
-        start = int(request.values.get('start'))
-    else:
-        start = 1
-    if request.values.get('limit'):
-        limit = int(request.values.get('limit'))
-    else:
-        limit = 20
-    count = len(results)
     paginated = {}
-    if count < start:
-        paginated['previous_link'] = ''
-        paginated['next_link'] = ''
-    else:
-        if start == 1:
-            paginated['previous_link'] = ''
+
+    try:
+        if request.values.get('page'):
+            page = int(request.values.get('page'))
         else:
-            previous_start = max(1, start - limit)
-            previous_limit = start - 1
-            paginated['previous_link'] = url + '?start=%d&limit=%d' % \
-                    (previous_start, previous_limit)
-        if start + limit > count:
+            page = 1
+        if request.values.get('limit'):
+            limit = int(request.values.get('limit'))
+        else:
+            limit = 10
+
+        count = len(results)
+        start = page * limit - limit + 1
+
+        paginated['is_good_query'] = True
+
+        if count < start:
+            paginated['previous_link'] = ''
             paginated['next_link'] = ''
         else:
-            next_start = start + limit
-            paginated['next_link'] = url + '?start=%d&limit=%d' % (next_start, limit)
-    paginated['results'] = results[(start - 1):(start - 1 + limit)]
+            if page == 1:
+                paginated['previous_link'] = ''
+            else:
+                paginated['previous_link'] = url + '?page=%d&limit=%d' % (page - 1, limit)
+            if start + limit > count:
+                paginated['next_link'] = ''
+            else:
+                paginated['next_link'] = url + '?page=%d&limit=%d' % (page + 1, limit)
+        paginated['results'] = results[(start - 1):(start - 1 + limit)]
+    except ValueError:
+        paginated['is_good_query'] = False
     return paginated
