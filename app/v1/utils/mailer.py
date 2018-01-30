6e9b4f09e70a0569f8e5966eb28e5d45a58d0a46
pndemo <ndemopaul1@gmail.com>
2018-01-30 12:51:53 +0300

    [Chore] Update email sending function'

--- app/v1/utils/mailer.py
+++ app/v1/utils/mailer.py
@@ -1,18 +1,18 @@
 """ Settings and function used to send emails to users """
 
-import requests
+from flask_mail import Mail, Message
+from app import create_app
 
-URL = 'https://api.mailgun.net/v3/sandbox362fd54f9ecc4009863808af412cd1b0.mailgun.org'
+# pylint: disable=C0103
+# pylint: disable=W0703
 
-API_KEY = 'key-3b5108bc69b6e4d55077afc8e4b72731'
-
-FROM = 'Yummy Recipes Inc. <postmaster@sandbox362fd54f9ecc4009863808af412cd1b0.mailgun.org>'
+app = create_app('development')
+mail = Mail(app)
 
 def send_mail(user, subject, text):
     """ Return True if email successfully sent, otherwise return False """
-    try:
-        requests.post(URL, auth=("api", API_KEY), data={"from":FROM, "to":user.email, \
-                "subject":subject, "text":text})
-        return True
-    except Exception:
-        return False
+    msg = Message(subject, sender='hckalii2018@gmail.com', \
+	        recipients=[user.email])
+    msg.body = text
+    mail.send(msg)
+    return True
