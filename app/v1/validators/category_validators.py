850b3ff1da63c2a7aae313f4976d2f99a81342c1
pndemo <ndemopaul1@gmail.com>
2018-02-06 10:58:56 +0300

    [Chore] Category name validation

--- app/v1/validators/category_validators.py
+++ app/v1/validators/category_validators.py
@@ -3,7 +3,7 @@
 from app.v1.models.category_models import Category
 from app.v1.validators import validate_title
 
-def validate_category_name(value, user, category_id=None):
+def validate_category_name(value, user_id, category_id=None):
     """
     Returns 'Valid' if category name is valid and category with similar category name
     has not been created by specific user or is related to specific category id related
@@ -14,9 +14,9 @@ def validate_category_name(value, user, category_id=None):
     elif not validate_title(value):
         return 'Please enter a valid category name.'
     else:
-        categories = Category.query.filter_by(user_id=user.id)
+        categories = Category.query.filter_by(user_id=user_id)
         for category in categories:
-            if category.user_id == user.id and category.category_name.lower() == \
+            if category.user_id == user_id and category.category_name.lower() == \
                     value.lower():
                 if category_id and category.category_id == category_id:
                     return 'Valid'
