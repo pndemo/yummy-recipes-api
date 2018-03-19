9773dc5b70cf14acb26561616d9350dd8a8a2a65
pndemo <ndemopaul1@gmail.com>
2018-03-19 12:31:53 +0300

    [Fix] CORS requests

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 """ Running application configuration """
 
 from app import create_app
+from flask_cors import CORS
 
 # pylint: disable=C0103
 
 config_name = 'development'
 app = create_app(config_name)
+CORS(app)
 
 if __name__ == '__main__':
     app.run()
